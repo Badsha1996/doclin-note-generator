8ec1badd5dbfa789c5afaaa28f98c070324a50fc
Badsha Laskar <badshalaskar97@outlook.com>
2025-07-14 15:32:04 +0530

    folder restructure

--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI,Depends
 from sqlalchemy.orm import Session
-from .auth.infrastructure.database import Base, engine, SessionLocal
-from .auth.infrastructure.models import UserModel
+from .database.database import Base, engine, SessionLocal
+from .infrastructure.models import UserModel
 
-
+# main APP initiation 🎌
 app = FastAPI(
     title="Doclin Note Generator",
     description="Doclin note generator is an app which will make your notes and question making easy with advanced AI.",
     version="1.1.1"
 )
 
-# CREATE the actual table :accordion:
+# CREATE the actual table 🔢
 Base.metadata.create_all(bind=engine)
+
+# Parent route for prefix added
+
 
 @app.get("/")
 async def root():
