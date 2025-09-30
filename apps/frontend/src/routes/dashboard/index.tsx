9fa8b8be2ef3929c1dc2b9c8a0926b384ecdf4df
ANIRUDHA PRADHAN <anirudhapradhan403@gmail.com>
2025-09-30 22:23:25 +0530

    redirect to login page with message if user try to use protected feature

--- apps/frontend/src/routes/dashboard/index.tsx
+++ apps/frontend/src/routes/dashboard/index.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { getUserInfo } from "@/lib/auth";
 import DashboardPage from "@/components/pages/DashboardPage";
+import { toast } from "sonner";
 export const Route = createFileRoute("/dashboard/")({
   beforeLoad: () => {
     const user = getUserInfo();
     if (!user || user?.role === "user") {
-      throw redirect({ to: "/" });
+      toast.error("This feature is only available for admin users.");
+      throw redirect({ to: "/login" });
     }
   },
   component: DashboardPage,
--- apps/frontend/src/routes/examPaper/index.tsx
+++ apps/frontend/src/routes/examPaper/index.tsx
@@ -2,14 +2,13 @@ import ExamPaperPage from "@/components/pages/ExamPaperPage";
 import { getUserInfo } from "@/lib/auth";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
-
+import { toast } from "sonner";
 export const Route = createFileRoute("/examPaper/")({
   beforeLoad: () => {
     if (!getUserInfo()) {
-      throw redirect({ to: "/" });
+      toast.error("You have to login first in order to access this feature.");
+      throw redirect({ to: "/login" });
     }
   },
   component: ExamPaperPage,
 });
-
-
