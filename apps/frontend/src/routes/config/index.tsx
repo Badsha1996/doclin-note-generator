26ebc663825adf59bf51cc2348b8df6b143776aa
ANIRUDHA PRADHAN <114860976+Anirudha0702@users.noreply.github.com>
2025-10-01 12:35:08 +0530

    Dng fix/remove unused imports (#43)
    * changes
    
    * Api response fix and login page see password
    
    * change subject  for email verification
    
    * Rename users.tsx  CASE.tsx
    
    * Rename users.tsx  to Users.tsx
    
    * redirect to login page with message if user try to use protected feature

--- apps/frontend/src/routes/config/index.tsx
+++ apps/frontend/src/routes/config/index.tsx
@@ -2,14 +2,13 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { getUserInfo } from "@/lib/auth";
 import ConfigPage from "@/components/pages/ConfigPage";
-
+import { toast } from "sonner";
 export const Route = createFileRoute("/config/")({
   beforeLoad: () => {
     if (!getUserInfo()) {
-      throw redirect({ to: "/" });
+      toast.error("You have to login first in order to access this feature.");
+      throw redirect({ to: "/login" });
     }
   },
   component: ConfigPage,
 });
-
-
