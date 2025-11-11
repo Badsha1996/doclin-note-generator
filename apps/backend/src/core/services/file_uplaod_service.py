2c1193d2788fab7c246e8e8c7623840ef58dcc05
ANIRUDHA PRADHAN <anirudhapradhan403@gmail.com>
2025-11-11 22:12:41 +0530

    Enhance file upload service with large file size validation and improve error handling in issue reporting

--- apps/backend/src/core/services/file_uplaod_service.py
+++ apps/backend/src/core/services/file_uplaod_service.py
@@ -4,7 +4,7 @@ import shutil
 import cloudinary
 import cloudinary.uploader
 from fastapi import UploadFile, File
-from ...utils.exceptions import InternelServerException
+from ...utils.exceptions import InternelServerException, LargePayloadException
 from ...config.config import settings
 class FileUploadService:
     def __init__(self):
@@ -12,6 +12,8 @@ class FileUploadService:
         self.api_key = settings.CLOUDINARY_API_KEY
         self.api_secret = settings.CLOUDINARY_API_SECRET
         self.upload_dir = "temp_uploads"
+        self.MAX_FILE_SIZE_MB = 5
+        self.MAX_FILE_SIZE = self.MAX_FILE_SIZE_MB * 1024 * 1024
         os.makedirs(self.upload_dir, exist_ok=True)
         cloudinary.config(
             cloud_name=self.cloud_name,
@@ -22,14 +24,21 @@ class FileUploadService:
     async def upload_file(self, file: UploadFile = File(...)) -> str:
         temp_file_path = os.path.join(self.upload_dir, file.filename)
         try:
+            contents = await file.read()
+            if len(contents) > self.MAX_FILE_SIZE:
+                raise LargePayloadException(
+                    detail=f"File size exceeds {self.MAX_FILE_SIZE_MB} MB limit"
+                )
+
             with open(temp_file_path, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
+                buffer.write(contents)
         finally:
             file.file.close()
         try:
             upload_result = cloudinary.uploader.upload(temp_file_path,resource_type="image", 
             folder="pdf_uploads",
-            public_id=os.path.splitext(file.filename)[0],
+            use_filename=True,
+            unique_filename=True,
             eager=[{"format": "jpg", "page": 1, "width": 500}], 
             eager_async=False,)
             file_url = upload_result.get("secure_url")
